62778b48ae316272d765df08819bf1fb13f9e094
Suriya <mcmio125@gmail.com>
2018-09-26 23:16:53 +0700

    Fix barcode

--- app/src/main/java/com/yms/activity/ScanBarActivity.java
+++ app/src/main/java/com/yms/activity/ScanBarActivity.java
@@ -1,75 +1,127 @@
 package com.yms.activity;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.google.zxing.Result;
 import com.yms.R;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import java.io.IOException;
 
-public class ScanBarActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+public class ScanBarActivity extends AppCompatActivity {
 
     //REQUEST CODE
     private static final int REQEUST_CAMERA = 1001;
-
-    //SharedPreferences file
-    private static final String SPF_URL = "vidsurl";
-    private static final String URL = "url";
-
-    private ZXingScannerView scannerView;
-    private String url;
+    private SurfaceView cameraView;
+    private BarcodeDetector barcode;
+    private CameraSource cameraSource;
+    private SurfaceHolder holder;
+    private TextView tvScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        scannerView = new ZXingScannerView(this);
-        setContentView(scannerView);
-
-        SharedPreferences urlPreferences = getSharedPreferences(SPF_URL, Context.MODE_PRIVATE);
-        url = urlPreferences.getString(URL, "");
-        if (url.isEmpty()) {
-            Intent settingIntent = new Intent(ScanBarActivity.this, SettingActicity.class);
-            startActivity(settingIntent);
-        }
-
-        if (checkPermission()) {
-
-        } else {
-            requestPermission();
-        }
-
+        setContentView(R.layout.activity_scan_qr);
+        initInstace();
     }
 
-    @Override
-    public void handleResult(Result result) {
-        String scanResult = result.getText().toString();
-        Intent intent = new Intent();
-        intent.putExtra("barcode", scanResult);
-        setResult(RESULT_OK, intent);
-        finish();
+    private void initInstace() {
+
+        cameraView = findViewById(R.id.cameraView);
+        cameraView.setZOrderMediaOverlay(true);
+        tvScan = findViewById(R.id.tvScan);
+        holder = cameraView.getHolder();
+        barcode = new BarcodeDetector.Builder(ScanBarActivity.this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+        if (!barcode.isOperational()) {
+            showToast("Sorry, Couldn't setup the detector");
+            finish();
+        }
+        cameraSource = new CameraSource.Builder(ScanBarActivity.this, barcode)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(24)
+                .setAutoFocusEnabled(true)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(ScanBarActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermission();
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcode.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodeArray = detections.getDetectedItems();
+                if (barcodeArray.size() != 0) {
+                    tvScan.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(ScanBarActivity.this, MainActivity.class);
+                            intent.putExtra("barcode", barcodeArray.valueAt(0).displayValue);
+                            setResult(RESULT_OK, intent);
+                            finish();
+                            cameraSource.stop();
+                        }
+                    });
+                }
+            }
+        });
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQEUST_CAMERA: {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    return;
+                    if (checkPermission()) {
+                        return;
+                    }
+                    try {
+
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             }
@@ -77,22 +129,9 @@ public class ScanBarActivity extends AppCompatActivity implements ZXingScannerVi
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (checkPermission()){
-            if (scannerView == null){
-                scannerView = new ZXingScannerView(ScanBarActivity.this);
-                setContentView(scannerView);
-            }
-            scannerView.setResultHandler(this);
-            scannerView.startCamera();
-        }
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
-        scannerView.stopCamera();
+        cameraSource.release();
     }
 
     private boolean checkPermission() {
@@ -101,5 +140,8 @@ public class ScanBarActivity extends AppCompatActivity implements ZXingScannerVi
 
     private void requestPermission() {
         ActivityCompat.requestPermissions(ScanBarActivity.this, new String[]{Manifest.permission.CAMERA}, REQEUST_CAMERA);
+    }
+
+    private void showToast(String s) {
     }
 }
